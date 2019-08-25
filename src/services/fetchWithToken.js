df6e4f8af587892e09f5c6eee88a350cbe82e070
Stas R <rar.stas@gmail.com>
2019-08-25 23:42:59 +0300

    update

--- src/services/fetchWithToken.js
+++ src/services/fetchWithToken.js
@@ -1,40 +1,58 @@
 const fetch = require('cross-fetch');
 const tokenService = require('./token');
-var counterRequest = 0;
-const rejectNotAuthorizedRequestText = 'Available number of requests exceeded';
+const { isTokenExpire } = require('../helpers');
+const queueRequests = [];
+var busy = false;
+var tokenTry = 0;
 
 module.exports = async function fetchWithToken(url, options = {}){
+    let fetchWithTokenResolve, fetchWithTokenReject;
+    const requestPromise = new Promise((resolve, reject) => {
+        fetchWithTokenResolve = resolve;
+        fetchWithTokenReject = reject;
+    });
+
+    const requestFn = () => {
+        let token = tokenService.get();
+        options.headers = {
+            ...options.headers,
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/vnd.api+json',
+            'Accept': 'application/vnd.api+json'
+        };
+
+        fetch(url, options).then(async response => {
+            try {
+                fetchWithTokenResolve(response);
+            }
+            catch (err) {
+                fetchWithTokenReject(err);
+            }
+        }).catch((err) => {
+            fetchWithTokenReject(err);
+        });
+    };
+
     let token = tokenService.get();
 
-    options.headers = {
-        ...options.headers,
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/vnd.api+json',
-        'Accept': 'application/vnd.api+json'
+    if(isTokenExpire(token)) {
+        token = null;
     }
 
-    return new Promise((resolve, reject) => {
-        fetch(url, options).then(async response => {
-            try{
-                if(response.status === 401){
-                    await tokenService.update();
-                    if(counterRequest < 2){
-                        counterRequest++;
-                        resolve(fetchWithToken(url, options));
-                    }
-                    else{
-                        counterRequest = 0;
-                        throw rejectNotAuthorizedRequestText;
-                    }
-                }
-                else{
-                    counterRequest = 0;
-                    resolve(response);
-                }
-            }
-            catch(err){
-                reject(err);
-            }
-        });
-    });
+    if(!token || busy){
+        busy = true;
+        queueRequests.push(requestFn);
+        if(!token && !tokenTry) {
+            tokenTry++;
+            token = await tokenService.update();
+            busy = false;
+            tokenTry = 0;
+            executeQueue();
+        }
+    }
+    else{
+        requestFn();
+    }
+
+    return requestPromise;
 }
